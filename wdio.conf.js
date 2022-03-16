8de776b474a3c4af09daddb61ec337ceac6e04a5
Lucas Romero <lromero@enia.io>
2022-03-16 15:15:06 -0300

    last

--- wdio.conf.js
+++ wdio.conf.js
@@ -57,7 +57,6 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
-        acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
